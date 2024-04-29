06ed4797f4bf2ae48741595074ee39fcd344b498
Evelina <kardash.evelina@chnu.edu.ua>
2024-04-29 10:19:43 +0300

    abb

---
+++ OOPLab5T/Task_lab_5.h
@@ -0,0 +1,14 @@
+//
+// Created by Eveline on 27.04.2024.
+//
+
+#ifndef UNTITLED_TASK_LAB_5_H
+#define UNTITLED_TASK_LAB_5_H
+
+int mainTask1();
+int mainTask2();
+int mainTask3();
+
+#endif //UNTITLED_TASK_LAB_5_H
+
+
